eeb2d1b2e4808b925aaccb979dad58056b5daf68
Baldassare <baldassarecinao@gmail.com>
2017-03-02 13:26:30 -0400

    Configuracion del proyecto

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Blade::setContentTags('<%', '%>');
+Blade::setEscapedContentTags('<%%', '%%>');
 
 Route::get('/', function () {
     return view('welcome');
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,40 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // compila el archivo app.scss
+    mix.sass('app.scss')
+        // Copia las fuentes al directorio /public
+        .copy(
+            [
+                'bower_components/bootstrap-sass-official/assets/fonts',
+                'bower_components/font-awesome/fonts'
+            ],
+            'public/fonts'
+        )
+        // Copia los archivos .js de jquery y bootstrap a resources/assets/js/vendor
+        .copy(
+            [
+                'bower_components/jquery/dist/jquery.js',
+                'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+                'bower_components/angular/angular.js',
+            ],
+            'resources/assets/js/vendor'
+        )
+        // Compila todos los .js dentro de public/app.js
+        .scripts(
+            [
+                'resources/assets/js/vendor/jquery.js',
+                'resources/assets/js/vendor/bootstrap.js',
+                'resources/assets/js/vendor/angular.js',
+                'resources/assets/js/app.js'
+            ],
+            'public/js/app.js'
+        )
+        // Opcional: crea una version para cada compilado dentro de public/build
+        .version(
+            [
+                "public/css/app.css",
+                "public/js/app.js"
+            ]
+        );
 });
