e10e5346035a57b22786e87d5adb1fb2d15d2589
Baldassare <baldassarecinao@gmail.com>
2017-03-13 08:35:20 -0400

    Limpiar comentarios y actualizar librerias js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,31 +8,3 @@ angular.module('app', [])
   apiKey: "9a0711e83b5b00c91e118c1211e7bdfa",
   apiUrl: 'http://gateway.marvel.com/v1/public/'
 });
-
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: 3000
-});
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-/*
-$('#carousel-comics').carousel({
-  interval: 3000
-});
-
-$('.carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  }
-  else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
-*/
